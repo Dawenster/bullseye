0e3b792ac98e33b07ffefa4ea71d2b4ab611d4fb
Dawenster <dstwen@gmail.com>
2013-07-02 10:36:55 -0700

    Initial Commit

---
+++ BullsEye/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  BullsEye
+//
+//  Created by David Wen on 2013-07-02.
+//  Copyright (c) 2013 David Wen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ BullsEye/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  BullsEye
+//
+//  Created by David Wen on 2013-07-02.
+//  Copyright (c) 2013 David Wen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
